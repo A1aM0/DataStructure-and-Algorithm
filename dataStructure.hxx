f0fdc6c0fa50517bca958623645ce2c61495250c
Songhao Yang <yoyoyanghao@126.com>
2020-03-29 15:25:19 +0800

    增加双向链表结构

--- dataStructure.hxx
+++ dataStructure.hxx
@@ -5,6 +5,12 @@ struct ListNode {
         int val;
         ListNode* next;
         ListNode(int x) : val(x), next(nullptr) {}
-    };
+};
+
+struct DoublyListNode {
+    int val;
+    DoublyListNode *next, *prev;
+    DoublyListNode(int x) : val(x), next(NULL), prev(NULL) {}
+};
 
 #endif
