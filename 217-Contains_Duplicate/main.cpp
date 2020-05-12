ffdc268d682c0a115ba3fd794a8ebea8ad24e117
Songhao Yang <yoyoyanghao@126.com>
2020-05-12 14:28:38 +0800

    217 done

--- 217-Contains_Duplicate/main.cpp
+++ 217-Contains_Duplicate/main.cpp
@@ -8,19 +8,24 @@
 
 #include <iostream>
 #include <vector>
-#include <set>
+#include <unordered_set>
 using namespace std;
 
 class Solution {
 public:
     static bool containsDuplicate(vector<int>& nums) { // 先排序，再一一对应过去。或者哈希表。
+        unordered_set<int> numHash; // 集合
+        for (int num : nums) {
+            if (numHash.count(num) > 0) return true;
+            numHash.insert(num);
+        }
         return false;
     }
 };
 
 int main() {
-//    vector<int> nums = {1, 2, 3, 1}; // output is true
-    vector<int> nums = {1, 2, 3, 4}; // output is false
+    vector<int> nums = {1, 2, 3, 1}; // output is true
+//    vector<int> nums = {1, 2, 3, 4}; // output is false
 //    vector<int> nums = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2}; // output is true
 //    vector<int> nums = {3, 1};
     cout << "result is " << Solution::containsDuplicate(nums) << endl;
