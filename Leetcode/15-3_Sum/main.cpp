20871b234fab640359bd92ec121acb26479f3951
Songhao Yang <yoyoyanghao@126.com>
2020-06-05 12:37:55 +0800

    15-三数之和

---
+++ Leetcode/15-3_Sum/main.cpp
@@ -0,0 +1,49 @@
+/*!
+ * @brief 三数之和
+ *
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+ * 注意：答案中不可以包含重复的三元组。
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        if (nums.size() < 3) return res;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1, right = nums.size() - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    res.push_back({nums[i], nums[left], nums[right]});
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                }
+                else if (sum > 0) right--;
+                else left++;
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    vector<int> nums{-1, 0, 1, 2, -1, -4};
+    Solution s;
+    vector<vector<int>> res = s.threeSum(nums);
+    for (auto row : res) {
+        for (auto num : row) cout << num << " ";
+        cout << endl;
+    }
+    return 0;
+}
